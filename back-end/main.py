82c196a0594a5794bcac5195df61f77887ad64a6
Alessio_Fischetti <alessio.fischetti@youbidata.com>
2026-02-05 19:26:17 +0100

    Creato collegamento ed API per comunicare con il bot OpenAI

--- back-end/main.py
+++ back-end/main.py
@@ -1,13 +1,36 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
+
+import os
+from openai import OpenAI, BaseModel
+
+load_dotenv()
+
+client = OpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key=os.environ["HF_TOKEN"],
+)
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Ciao, FastAPI funziona!"}
 
-def main():
-    print("Progetto Pyhton inizializzato")
+class RichiestaUtente(BaseModel):
+    richiesta: str
 
-# Solo se eseguito come script principale
-if __name__ == "__main__":
-    main()
+
+@app.post("/chatBot/userRequest")
+def read_root(data: RichiestaUtente):
+    return chiedi_al_bot(data)
+
+
+def chiedi_al_bot(data: RichiestaUtente):
+    completion = client.chat.completions.create(
+        model="allenai/Olmo-3.1-32B-Instruct:publicai",
+        messages=[
+            {
+                "role": "user",
+                "content": data.richiesta
+            }
+        ]
+    )
+
+    return completion.choices[0].message
